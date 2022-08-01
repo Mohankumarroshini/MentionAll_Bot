7730e56b7e0fccbd1d589d9ce35b5ef5d9404cd8
ImKarnan <109649877+ImKarnan@users.noreply.github.com>
2022-08-01 21:01:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,8 +97,7 @@ async def mentionall(event):
     if usr.deleted:
        continue
     usrnum += 1
-    usrtxt += usr.username if usr.username else 
-    continue
+    usrtxt += usr.username if usr.username else continue
     if usrnum == 1:
       if mode == "text_on_cmd":
         txt = f"{usrtxt}\n\n{msg}"
