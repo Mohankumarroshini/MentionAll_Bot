1761209150cfcf903bbad467b95972d8f2fb7b59
ImKarnan <109649877+ImKarnan@users.noreply.github.com>
2022-08-01 18:09:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,8 +94,8 @@ async def mentionall(event):
   async for usr in client.iter_participants(chat_id):
     if not chat_id in spam_chats:
       break
-if usr.deleted:
-   continue
+    if usr.deleted:
+       continue
 usrnum += 1
     usrtxt += f"@{usr.username}"
     if usrnum == 1:
